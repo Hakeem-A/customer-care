89967ecd1849f04b293d6bdca6b6a5e1e49ca30d
Abdi Mohamed <bashanesix@gmail.com>
2025-08-05 11:57:00 +0300

    adding code and tailwindcss

---
+++ customer-care/src/pages/CustomerDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CustomerDetail = () => {
+  return (
+    <div>
+      <h2>Customer Detail</h2>
+      <p>This page is under construction.</p>
+    </div>
+  )
+}
+
+export default CustomerDetail
